4a94ffce9e6b66b60fe08c2b8bdab0593a3f0526
Cristian Amaral <cristianmaralsilva@gmail.com>
2017-10-22 18:48:50 -0200

    Diretiva define implementada para controlar o modo análise

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ANALISE
+//#undef ANALISE
+
 int main()
 {
     FILE *arquivoDeEntrada = NULL;
@@ -12,20 +15,26 @@ int main()
     int opcao, linhas, colunas;
 
     /* Repetir até que a opção 3 seja escolhida - finaliza o programa */
+
     while (1) {
         /* MENU */
-        printf("+-------------------------------------+\n");
-        printf("|        Programa do Labirinto        |\n");
-        printf("+-------------------------------------+\n");
-        printf("|                 MENU                |\n");
-        printf("+-------------------------------------+\n");
-        printf("| 1 -> Carregar novo arquivo de dados |\n");
-        printf("| 2 -> Processar e exibir respostas   |\n");
-        printf("| 3 -> Sair do programa               |\n");
-        printf("+-------------------------------------+\n\n");
+        printf("+--------------------------------------+\n");
+        #ifndef ANALISE
+            printf("|        Programa do Labirinto         |\n");
+        #else
+            printf("| Programa do Labirinto - Modo Analise |\n");
+        #endif
+        printf("+--------------------------------------+\n");
+        printf("|                 MENU                 |\n");
+        printf("+--------------------------------------+\n");
+        printf("| 1 -> Carregar novo arquivo de dados  |\n");
+        printf("| 2 -> Processar e exibir respostas    |\n");
+        printf("| 3 -> Sair do programa                |\n");
+        printf("+--------------------------------------+\n\n");
 
         printf("Entre com a opcao desejada: ");
         scanf("%d", &opcao);
+        system("cls");
         switch (opcao) {
             case 1:
                 do {
@@ -50,7 +59,12 @@ int main()
                 }
                 else {
                     printf("Calculando um possivel caminho para o cachorro...\n");
-                    solucionaLabirinto2(labirinto, linhas, colunas);
+                    #ifndef ANALISE
+                        solucionaLabirinto(labirinto, linhas, colunas);
+                    #else
+                        solucionaLabirinto2(labirinto, linhas, colunas);
+                    #endif
+                    printf("\n");
                 }
                 /* Resetando as visitas para que seja possível se o usuário desejar calcular */
                 /* o caminho novamente com o mesmo arquivo de entrada */
